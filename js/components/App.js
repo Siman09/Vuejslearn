b8fff536e8d26f91f1f8c66ad984d62f26d99804
siman09 <simanjitsingh600@gmail.com>
2024-09-16 14:03:44 +0530

    add tag buttons

--- js/components/App.js
+++ js/components/App.js
@@ -1,15 +1,18 @@
-import buttons from "./AppButton.js";
+import AppButton from "./AppButton.js";
 import inputs from "./Appinput.js";
+
 export default
     {
 
         // <button class="rounded mx-2 my-2 py-1 px-2 bg-red-400 hover:bg-red-500"  v-on:click="go">Add</button>
         template:
             `
-            <AppInput @inherited="go"></AppInput><br><br>
-        <h3 class="font-bold py-2"  >In Progress</h3>
+            <AppInput @inherited="go"> </AppInput><br><br>
+            
+        <AppButton v-for="tag in tags" v-on:click="runn(tag)" type='secondary'>{{tag}}</AppButton>
+        <h3 class="font-bold py-2">In Progress</h3>
         <ul>
-            <li class="border px-2 py-2" v-for="assignment in inprogress" :key="assignment.id">{{ assignment.name }}<input v-model="assignment.Complete" class="mx-2" type="checkbox"></input></li>
+            <li class="border px-2 py-2" v-for="assignment in tagfilter" :key="assignment.id">{{ assignment.name }}<input v-model="assignment.Complete" class="mx-2" type="checkbox"></input></li>
         </ul>
         <h3 class="font-bold pt-2" v-show="assignments.filter(a=>a.Complete).length>0"> Completed assignments<h3>
         <ul>
@@ -20,8 +23,9 @@ export default
     `,
         components: {
 
-            AppButton: buttons,
+      
             AppInput: inputs,
+            AppButton: AppButton,
         },
         computed: {
             inprogress() {
@@ -29,17 +33,33 @@ export default
             },
             inCompleted() {
                 return this.assignments.filter(a => a.Complete);
+            },
+            tagfilter(){
+                if(this.settag==='all')
+                {
+                    return this.assignments;
+                }
+                else{
+                    return this.assignments.filter(a=>a.tag===this.settag);
+                }
             }
         },
 
         data() {
             return {
                 assignments: [
-                    { name: 'Finish Project', Complete: false, id: 1 },
-                    { name: 'Read Chapter 4', Complete: false, id: 2 },
-                    { name: 'turn in Home work', Complete: false, id: 3 }
+                    { name: 'Read Chapter 1', Complete: false, id: 1,tag:'Math' },
+                    { name: 'Read Chapter 2', Complete: false, id: 2,tag:'Math' },
+                    { name: 'Read Chapter 3', Complete: false, id: 3,tag:'English' },
+                    { name: 'Read Chapter 4', Complete: false, id: 4,tag:'English' },
+                    { name: 'Read Chapter 5', Complete: false, id: 5,tag:'Computer' },
+                   
                 ],
                 enterdata: '',
+                settag:'',
+                tags:[
+                    'all','Math','English','Computer'
+                ],
             };
         },
 
@@ -51,10 +71,13 @@ export default
                 
                         this.assignments.push({ name: obj, complete: false, id: this.assignments.length + 1 });
                         // this.obj = '';
-                        alert('Enter Sucessfully');
+                        // alert('Enter Sucessfully');
                     
                 }, 1);
 
+            },
+            runn(tag){
+                this.settag=tag;
             }
 
 
--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -3,10 +3,10 @@ export default {
     template: `
     
        <button :class="{
-       'bg-red-400 text-white px-2 py-1 rounded mx-2': type==='primary',
-       'bg-blue-400 text-white px-2 py-1 rounded mx-2': type==='secondary',
-       'bg-green-400 text-white px-2 py-1 rounded mx-2': type==='success',
-       'bg-yellow-400 text-white px-2 py-1 rounded mx-2': type==='warning',
+       'bg-red-400 text-white px-2 py-1 rounded mx-2 hover:bg-red-500': type==='primary',
+       'bg-blue-400 my-2 text-white px-2 py-1 rounded mx-2 hover:bg-red-500': type==='secondary',
+       'bg-green-400 text-white px-2 py-1 rounded mx-2 hover:bg-red-500': type==='success',
+       'bg-yellow-400 text-white px-2 py-1 rounded mx-2 hover:bg-red-500': type==='warning',
        }"> <slot /><button>
     `,
     data() {
